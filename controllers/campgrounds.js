7a4a3d07d98b055b03315a708b46c4f6953e67ae
Joe Errington <jerrington2010@gmail.com>
2023-02-01 21:03:11 +1100

    revert

--- controllers/campgrounds.js
+++ controllers/campgrounds.js
@@ -1,6 +1,6 @@
 const Campground = require('../models/campground');
 const mbxGeocoding = require("@mapbox/mapbox-sdk/services/geocoding");
-const mapToken = process.env.MAPBOX_TOKEN;
+const mapBoxToken = process.env.MAPBOX_TOKEN;
 const geocoder = mbxGeocoding({ accessToken: mapBoxToken });
 const { cloudinary } = require("../cloudinary");
 
