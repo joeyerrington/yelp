8b028eee51c412113a857cd100925807edbcc1b4
Joe Errington <jerrington2010@gmail.com>
2023-02-01 21:25:08 +1100

    recentre map

--- public/javascripts/clusterMap.js
+++ public/javascripts/clusterMap.js
@@ -2,7 +2,7 @@ mapboxgl.accessToken = mapToken;
 const map = new mapboxgl.Map({
     container: 'cluster-map',
     style: 'mapbox://styles/mapbox/light-v10',
-    center: [-103.59179687498357, 40.66995747013945],
+    center: [133.8765127, -23.7004799],
     zoom: 3
 });
 
